1b81f1d2df343db64ed3b14f1701fa6ab810918b
do7be <bomsap700@gmail.com>
2023-12-13 22:26:10 +0900

    Update 2-06-12.ts

--- 2-06-12.ts
+++ 2-06-12.ts
@@ -1,4 +1,4 @@
 type Chainable<T extends { [key: string]: any } = {}> = {
-  option<U extends string, P extends any>(key: U, value: P): Chainable<{ [K in keyof T | U]: K extends U ? P : T[K] }>,
+  option<U extends string, P>(key: U, value: P): Chainable<{ [K in keyof T | U]: K extends U ? P : T[K] }>,
   get(): T
 }
