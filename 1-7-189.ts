13d836495ffff67e320db15c665e53ffbf55ae50
do7be <bomsap700@gmail.com>
2023-12-13 10:49:12 +0900

    Update 1-7-189.ts

--- 1-7-189.ts
+++ 1-7-189.ts
@@ -7,3 +7,11 @@ type MyAwaited<T> = T extends Promise<Promise<Promise<infer U>>>
   : T extends { then: (hoge: (args: infer U) => any) => any }
   ? U
   : never
+
+
+// 再帰ver
+type MyAwaited<T> = T extends Promise<infer U>
+  ? MyAwaited<U>
+  : T extends { then: (hoge: (args: infer U) => any) => any }
+  ? U
+  : T
