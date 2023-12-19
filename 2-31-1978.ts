45745dd63d71d8783d58b580f9f2768c994557a3
do7be <bomsap700@gmail.com>
2023-12-19 16:38:16 +0900

    Update 2-31-1978.ts

--- 2-31-1978.ts
+++ 2-31-1978.ts
@@ -7,3 +7,14 @@ type PercentageParser<A extends string> = A extends `+${infer B}` | `-${infer B}
 : A extends `${infer N}%`
   ? ['', N, '%']
   : ['', A, ''] 
+
+// or
+type PercentageParser<A extends string> = A extends `${infer F}${infer B}`
+? F extends '+' | '-'
+  ? B extends `${infer N}%`
+    ? [F, N, '%']
+    : [F, B, ''] 
+  : A extends `${infer N}%`
+    ? ['', N, '%']
+    : ['', A, ''] 
+: ['', '', '']
