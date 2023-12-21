cdf3dd0d723e584f8d5f30cc0b0d739817f5af76
do7be <bomsap700@gmail.com>
2023-12-21 23:42:43 +0900

    Update 2-37-2757.ts

--- 2-37-2757.ts
+++ 2-37-2757.ts
@@ -3,3 +3,7 @@ type PartialByKeys<T, K extends keyof T = keyof T> =  keyof T extends K
 : { [U in keyof Omit<T, K>]: T[U] } & { [U in K]?: T[U] } extends infer A
   ? { [P in keyof A]: A[P] }
   : never
+
+type PartialByKeys<T, K extends keyof T = keyof T> = { [U in keyof Omit<T, K>]: T[U] } & { [U in K]?: T[U] } extends infer A
+  ? { [P in keyof A]: A[P] }
+  : never
