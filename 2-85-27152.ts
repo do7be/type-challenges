7bd765e98500788ee45ee27741a91867457102d2
do7be <bomsap700@gmail.com>
2024-01-27 18:22:42 +0900

    Update 2-85-27152.ts

--- 2-85-27152.ts
+++ 2-85-27152.ts
@@ -1,7 +1,3 @@
-type Triangular<N extends number, S extends any[] = [], I extends any[] = [any], I2 extends any[] = []> = N extends 0
-? 0
-: I2['length'] extends I['length']
-? I['length'] extends N
-  ? S['length']
-  : Triangular<N, [...S, any], [...I, any], [any]>
-: Triangular<N, [...S, any], I, [...I2, any]>
+type Triangular<N extends number, S extends any[] = [], I extends any[] = []> = I['length'] extends N
+? S['length']
+: Triangular<N, [...S, ...I, any], [...I, any]>
