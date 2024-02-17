2476b13297006292dac8edb9bd217f5b83606768
do7be <bomsap700@gmail.com>
2024-02-17 13:19:18 +0900

    Create 3-23-1290.ts

---
+++ 3-23-1290.ts
@@ -0,0 +1,6 @@
+declare function defineStore<S, G, A>(store: {
+  id: string; 
+  state: (this: never) => S; 
+  getters: G & ThisType<Readonly<S> & { [K in keyof G]: G[K] extends () => infer R ? R : never }>;
+  actions: A & ThisType<S & A>
+}): S & { [K in keyof G]: G[K] extends () => infer R ? R : never } & A
