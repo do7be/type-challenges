3cea83914d6defa7a951e550f969dba04841236e
do7be <bomsap700@gmail.com>
2024-02-17 13:20:44 +0900

    refactoring

--- 3-23-1290.ts
+++ 3-23-1290.ts
@@ -1,6 +1,8 @@
+type Getters<G> = { [K in keyof G]: G[K] extends () => infer R ? R : never }
+
 declare function defineStore<S, G, A>(store: {
   id: string; 
   state: (this: never) => S; 
-  getters: G & ThisType<Readonly<S> & { [K in keyof G]: G[K] extends () => infer R ? R : never }>;
+  getters: G & ThisType<Readonly<S> & Getters<G>>;
   actions: A & ThisType<S & A>
-}): S & { [K in keyof G]: G[K] extends () => infer R ? R : never } & A
+}): S & Getters<G> & A
