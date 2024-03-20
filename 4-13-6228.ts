52f6d1075e29aa55c4888c888e234bed3e26033b
do7be <bomsap700@gmail.com>
2024-03-20 21:33:19 +0900

    Update 4-13-6228.ts

--- 4-13-6228.ts
+++ 4-13-6228.ts
@@ -6,9 +6,16 @@ type SetProperty<T, K extends PropertyKey, V> = {
   [P in (keyof T) | K]: P extends K ? V : P extends keyof T ? T[P] : never
 }
 
-type Token = any
+type SpecificToken = '{' | '}' | '[' | ']' | ':' | ',' | true | false | null
+type Token = SpecificToken | `${string}`
 type ParseResult<T, K extends Token[]> = [T, K]
-type Tokenize<T extends string, S extends Token[] = []> = Token[]
+type Tokenize<T extends string, S extends Token[] = []> = T extends `${infer T0}${infer TR}`
+? T0 extends SpecificToken
+  ? Tokenize<TR, [...S, T0]>
+  : Tokenize<TR, [...S, T0]> // TODO
+: S
+
 type ParseLiteral<T extends Token[]> = ParseResult<any, T>
 
 type Parse<T extends string> = Pure<ParseLiteral<Tokenize<T>>[0]>
+
